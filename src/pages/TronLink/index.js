1eca910304b82657cef7f08fd524c6e4f1c57758
yushicheng <1542874601@qq.com>
2022-06-14 15:18:54 +0800

    temp:临时节点标记 星期二 下午 3:18:54 六月 14日 2022 修改了 src/pages/TronLink/index.js

--- src/pages/TronLink/index.js
+++ src/pages/TronLink/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useCallback } from "react";
-import { Alert, Space, Button } from "antd";
+import { Alert, Space, Button, message } from "antd";
+import React, { useState, useEffect, useCallback } from "react";
 // import classnames from "classnames";
 // import propTypes from "prop-types";
 
@@ -10,32 +10,62 @@ import { Alert, Space, Button } from "antd";
 
 export default function TronLink(props) {
 
-  const handleCallback = useCallback(async (event) => {
-    if (event.data.message && event.data.message.action === "acceptWeb") {
-      let res = await tronWeb.contract().at("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t");
-      console.log(res);
-    }
+  const [access_status, set_access_status] = useState("未授权");
+
+  const handleConnectWallte = useCallback(async () => {
+    const { code } = await tronLink.request({ method: "tron_requestAccounts" });
+    if (code === 200) {
+      set_access_status("已授权");
+    } else {
+      set_access_status("未授权");
+    };
   }, []);
 
   useEffect(() => {
-    (async () => {
-      let res = await tronWeb.contract().at("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t");
-      const balanceOf = await res.balanceOf("TVNWR1ZEJkyai4AiBpadU8vDvMu54sYKey").call({ from: "TVNWR1ZEJkyai4AiBpadU8vDvMu54sYKey" });
-      console.log(tronWeb.toDecimal(balanceOf));
-    })();
-    // window.addEventListener("message", handleCallback);
-    // return () => window.removeEventListener("message", handleCallback);
+    window.addEventListener("tronLink#initialized", handleConnectWallte, { once: true });
+    return () => window.removeEventListener("tronLink#initialized", handleConnectWallte);
   }, []);
 
-  return (
-    <Space direction="vertical">
-      <Space>
-        <Button type="primary">连接钱包</Button>
-        <Button type="primary">测试支付 20 usdt</Button>
+  const handleQueryUsdt = useCallback(async () => {
+    const log_complate = message.loading("正在查询中... ...");
+    const USDT_CONTRACT = await tronWeb.contract().at("TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj");
+    const balanceOf = await USDT_CONTRACT.balanceOf("TVNWR1ZEJkyai4AiBpadU8vDvMu54sYKey").call();
+    console.log(tronWeb.toDecimal(balanceOf));
+    log_complate();
+  }, []);
+
+  const handleTestPayTrx = useCallback(async () => {
+    const txn = await tronWeb.trx.sendTransaction("TSnfU72F6EmLbobtjNTUxsjjApWpTS9LdF", 100000000);
+    console.log("txn==>", txn);
+  }, []);
+
+  const handleTestPayUsdt = useCallback(async () => {
+    const USDT_CONTRACT = await tronWeb.contract().at("TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj");
+    const tx_result = await USDT_CONTRACT.transfer("TSnfU72F6EmLbobtjNTUxsjjApWpTS9LdF", 20000000).send();
+    console.log(tx_result);
+  }, []);
+
+  if (access_status === "未授权") {
+    return (
+      <Button type="primary" onClick={handleConnectWallte}>连接钱包</Button>
+    )
+  }
+
+  if (access_status === "已授权") {
+    return (
+      <Space direction="vertical">
+        <Space>
+          <Button type="primary" onClick={handleQueryUsdt}>查询USDT余额</Button>
+          <Button type="primary" onClick={handleTestPayTrx}>测试支付 100 TRX</Button>
+          <Button type="primary" onClick={handleTestPayUsdt}>测试支付 20 USDT</Button>
+        </Space>
+        <Alert type="info" message={`当前账户名为:$`} />
       </Space>
-      <Alert type="info" message={`当前账户名为:$`} />
-    </Space>
-  )
+    )
+  }
+
+  return null;
+
 };
 
 
